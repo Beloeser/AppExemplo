3e3781181762b456c1be57d07050eeef1316006c
Bernardo Amaral <bernardoamaral@cpejr.com.br>
2025-06-27 14:23:51 -0300

    commit inicial

--- src/Hooks/usuario.js
+++ src/Hooks/usuario.js
@@ -4,15 +4,12 @@ import { CreateUsuario } from "../Services/endpoints";
 export function useCreateUsuario({ onSuccess = () => {}, onError = () => {} } = {}) {
   return useMutation({
     mutationFn: async (body) => {
-      console.log("[useCreateUsuario] mutate body:", body);
       return CreateUsuario(body);
     },
     onSuccess: (data) => {
-      console.log("[useCreateUsuario] onSuccess data:", data);
       onSuccess(data);
     },
     onError: (error) => {
-      console.error("[useCreateUsuario] onError error:", error);
       onError(error);
     },
   });
--- src/Services/api.js
+++ src/Services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const baseURL = "http://192.168.0.19:8000"; 
-console.log("[api] baseURL =", baseURL);
+const baseURL = "http://192.168.0.19:8000";
 
 const api = axios.create({ baseURL });
 
@@ -12,11 +11,9 @@ api.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    console.log("[api] interceptor, final config.headers:", config.headers);
     return config;
   },
   (error) => {
-    console.error("[api] interceptor error:", error);
     return Promise.reject(error);
   }
 );
